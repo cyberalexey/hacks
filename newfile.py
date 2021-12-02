52b83cbf7dd3579fc047bd9c1166383e628d5b4b
cyberalexey <80445401+cyberalexey@users.noreply.github.com>
2021-12-02 16:20:38 +0800

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,6 @@
+import random, time
+from colorama import init
+init()
+from colorama import Fore
+for i in range(10000000000):
+	print(Fore.GREEN, random.randint(0,999999999999999999999999999999999999999999999999999999999999999999))
